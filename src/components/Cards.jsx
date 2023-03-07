903b10b606d24e6002378f87bf00f4631e1372a4
EnzoMagurno <magurnoenzo31602@gmail.com>
2023-03-07 09:46:24 -0300

    show Rick on screen

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,6 +1,12 @@
 import Card from './Card';
 
-export default function Cards(props) {
-   const { characters } = props;
-   return <div></div>;
+export default function Cards({characters}) {
+   return (<div>
+      {
+         characters.map(characters=>{
+            <Card name={characters.name} species={characters.species} gender={characters.gender} image={characters.image} onClose={characters.onClose}
+            />
+         })
+      }
+   </div>)
 }
