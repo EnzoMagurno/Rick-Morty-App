424446e37a2f6348954ed48bdb6dd96c5f2b4a99
EnzoMagurno <magurnoenzo31602@gmail.com>
2023-03-08 09:36:10 -0300

    character Rick add to character object array

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import './App.css';
-import Card from './components/card/Card.jsx';
+// import Card from './components/card/Card.jsx';
 import Cards from './components/cards/Cards.jsx';
 import SearchBar from './components/searchBar/SearchBar.jsx';
-import characters, { Rick } from './data.js';
+import characters from './data.js';
 
 function App() {
   return (
     <div className="App" style={{ padding: '25px' }}>
-      <div>
-        <Card
-          name={Rick.name}
-          species={Rick.species}
-          gender={Rick.gender}
-          image={Rick.image}
-          onClose={() => window.alert(`Cerrando ${Rick.name}`)}
-        />
-      </div>
       <hr />
       <div>
         <Cards characters={characters} />
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,12 +1,13 @@
 //name species gender image onClose
+import styles from './Card.module.css'
 export default function Card({name,species,gender,image,onClose}) {
    return (
-      <div>
-         /* <button onClick={onClose}>X</button>
-         <h2>Name: {name}</h2>
-         <h2>Species: {species}</h2>
-         <h2>Gender: {gender}</h2>
-         <img  src={image} alt="" /> */
+         <div className={styles.container}>
+         /* <button className={styles.buttonX} onClick={onClose}>X</button>
+         <h2 className="">Name: {name}</h2>
+         <h2 className="">Species: {species}</h2>
+         <h2 className="">Gender: {gender}</h2>
+         <img className="image" src={image} alt="" /> */
       </div>
    );
 }
