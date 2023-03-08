62f99ada1f98d4c6bebd3a8a993dac3d8e1f8efe
EnzoMagurno <magurnoenzo31602@gmail.com>
2023-03-08 10:36:35 -0300

    css styles

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -3,8 +3,8 @@ import styles from './Card.module.css'
 export default function Card({name,species,gender,image,onClose}) {
    return (
       <div className={styles.container}>
-            <img className="image" src={image} alt="" /> 
          <button className={styles.buttonX} onClick={onClose}>X</button>
+            <img className="image" src={image} alt="" /> 
          <h2 className="">Name: {name}</h2>
          <h2 className="">Species: {species}</h2>
          <h2 className="">Gender: {gender}</h2>
