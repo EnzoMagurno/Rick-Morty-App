2cc7dd53de5ed88dfe9141b7a17b087469740435
EnzoMagurno <magurnoenzo31602@gmail.com>
2023-03-13 17:31:46 -0300

    changes into component

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,7 +1,21 @@
 //name species gender image onClose
 import style from './Card.module.css'
 import { Link } from 'react-router-dom';
-export default function Card({name,species,gender,image,onClose,id}) {
+
+// function Card({name,species,gender,image,onClose,id}) {
+//    return (
+//       <div className={style.container}>
+//          <button className={style.buttonX} onClick={()=>onClose(id)}>X</button>
+//          <img className="image" src={image} alt="" /> 
+//          <Link to={`detail/${id}`}>
+//             <h2>Name: {name}</h2>
+//          </Link>
+//          <h2 className="">Species: {species}</h2>
+//          <h2 className="">Gender: {gender}</h2>
+//       </div>
+//    );
+// }
+const Card = ({name,species,gender,image,onClose,id}) =>{
    return (
       <div className={style.container}>
          <button className={style.buttonX} onClick={()=>onClose(id)}>X</button>
@@ -14,3 +28,4 @@ export default function Card({name,species,gender,image,onClose,id}) {
       </div>
    );
 }
+export default Card
--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -14,6 +14,5 @@ const Nav = (props) =>{
     )
   }
 
-  
 
 export default Nav
